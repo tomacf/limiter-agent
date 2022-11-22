8430e59826e267b59712c5a13eda47fc434a6842
iskikavid home <the.dav@protonmail.com>
2022-11-22 21:39:59 +0100

    Container

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -11,6 +13,11 @@ const router = require("./src/router");
 
 async function main() {
   await client.connect();
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(cors());
+
   app.use(router);
   app.listen(port, () =>
     console.log(`Nice app listening at http://localhost:${port}`)
--- src/redisClient.js
+++ src/redisClient.js
@@ -1,5 +1,7 @@
 const redis = require("redis");
-const client = redis.createClient();
+const client = redis.createClient({
+  url: "redis://redis:6379",
+});
 
 client.on("error", function (err) {
   console.log("could not establish a connection with redis. " + err);
